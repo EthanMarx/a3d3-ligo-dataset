2aac922fdf0c0c284e83104fc9f8b6a11e501e10
William Benoit <william.benoit@dgx1.ligo-wa.caltech.edu>
2024-09-24 05:12:15 -0700

    Updated lock file and aframe commit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export AFRAME_BASE_DIR=/home/ethan.marx/projects/a3d3-ligo-dataset/data/
+export AFRAME_BASE_DIR=
 LAW_CONFIG_FILE=./config.cfg poetry run law run a3d3_ligo_dataset.pipeline.A3D3Dataset --dev --workers 5
